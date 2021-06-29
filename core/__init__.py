a314464adae1c9600a481434b4e7bc41157f20dc
flekystyley <flekystyley@gmail.com>
2021-06-29 23:38:40 +0900

    update core;

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-from core.core_simple import Variable
-from core.core_simple import Function
-from core.core_simple import using_config
-from core.core_simple import no_grad
-from core.core_simple import as_array
-from core.core_simple import setup_variable
-
-setup_variable()
