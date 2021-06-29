3116e4ac589bc2fcebef3ff94e1b3ac29c1d3700
flekystyley <flekystyley@gmail.com>
2021-06-29 23:38:56 +0900

    update nZero;

---
+++ nZero/__init__.py
@@ -0,0 +1,13 @@
+# =============================================================================
+from nZero.core import setup_variable
+from nZero.core import as_variable
+from nZero.core import as_array
+from nZero.core import no_grad
+from nZero.core import using_config
+from nZero.core import Function
+from nZero.core import Variable
+is_simple_core = True
+# =============================================================================
+
+
+setup_variable()
